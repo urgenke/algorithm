a3aff57a3356e5a35059cbae37037503f70bdd3e
mocun <qianhangkang@163.com>
2023-11-30 10:58:13 +0800

    commit

---
+++ src/doublepointer/DoublePointerMain.java
@@ -0,0 +1,27 @@
+package doublepointer;
+
+/**
+ * @author : mocun
+ * @since : 2023/11/30
+ */
+public class DoublePointerMain {
+
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int max = 0;
+
+        while (left < right) {
+            max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+
+
+
+}
