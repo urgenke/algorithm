e3168ca1b07c2b9ac8de5a60ac356f185af2e0e7
mocun <qianhangkang@163.com>
2023-12-12 23:52:13 +0800

    😄

--- src/hot100/Main.java
+++ src/hot100/Main.java
@@ -1,6 +1,9 @@
 package hot100;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author : mocun
@@ -35,8 +38,71 @@ public class Main {
         }
     }
 
+    public int maxArea(int[] height) {
+        int max = 0;
+        int i = 0;
+        int j = height.length - 1;
 
-    public static void main(String[] args) {
-        new Main().moveZeroes(new int[]{0, 1, 0, 3, 12});
+        while (i < j) {
+            max = Math.max(max, Math.min(height[i], height[j]) * (j - i));
+            if (height[i] <= height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return max;
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    j++;
+                    continue;
+                }
+                if (k < nums.length - 1 && nums[k] == nums[k + 1]) {
+                    k--;
+                    continue;
+                }
+
+                if (nums[i] + nums[j] + nums[j + 1] > 0) {
+                    break;
+                }
+
+                if (nums[i] + nums[k] + nums[k - 1] < 0) {
+                    break;
+                }
+
+                int v = nums[i] + nums[j] + nums[k];
+                if (v == 0) {
+                    res.add(List.of(nums[i], nums[j], nums[k]));
+                    j++;
+                } else if (v < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+
+        }
+
+        return res;
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+//        new Main().moveZeroes(new int[]{0, 1, 0, 3, 12});
+//        System.out.println(new Main().maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+//        System.out.println(new Main().threeSum(new int[]{0, 0, 0, 0}));
+
     }
 }
