5f1884c8fc72098cab4b2410aa0401b72e3865f1
mocun <qianhangkang@163.com>
2023-12-12 17:34:39 +0800

    commit

--- src/hot100/Main.java
+++ src/hot100/Main.java
@@ -23,7 +23,16 @@ public class Main {
 
 
     public void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[index++] = num;
+            }
+        }
 
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
     }
 
 
