1346e6d68cb328d0915af5ab288d71c06332aa47
mocun <qianhangkang@163.com>
2023-12-27 21:30:51 +0800

    😄

--- src/hot100/Main.java
+++ src/hot100/Main.java
@@ -908,6 +908,59 @@ public class Main {
     }
 
 
+    public boolean searchMatrix2(int[][] matrix, int target) {
+
+        int i = 0;
+        int j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            int value = matrix[i][j];
+            if (target == value) {
+                return true;
+            }
+            if (target < value) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        return false;
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        int mid;
+        while (i <= j) {
+            mid = (i + j) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return i;
+    }
+
+
+//    public boolean wordBreak(String s, List<String> wordDict) {
+//        if (s.isEmpty()) {
+//            return true;
+//        }
+//        if (wordDict.isEmpty()) {
+//            return false;
+//        }
+//
+//        int[] dp = new int[s.length()];
+//
+//
+//    }
+
+
     public static void main(String[] args) throws InterruptedException {
 //        new Main().moveZeroes(new int[]{0, 1, 0, 3, 12});
 //        System.out.println(new Main().maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
@@ -940,13 +993,16 @@ public class Main {
 //        new Main().merge(new int[][]{{2, 3}, {1, 6}, {8, 10}, {15, 18}});
 //        System.out.println(new Main().maximalSquare(new char[][]{{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}}));
 //        new Main().rotate(new int[]{-1, -100, 3, 99}, 2);
-        System.out.println(new Main().maxProduct(new int[]{2, 3, -2, 4}));
-        System.out.println(new Main().maxProduct(new int[]{2, 3, -2, 4, -100}));
-        System.out.println(new Main().maxProduct(new int[]{0, 2}));
+//        System.out.println(new Main().maxProduct(new int[]{2, 3, -2, 4}));
+//        System.out.println(new Main().maxProduct(new int[]{2, 3, -2, 4, -100}));
+//        System.out.println(new Main().maxProduct(new int[]{0, 2}));
 //        System.out.println(new Main().coinChange(new int[]{2}, 3));
 //        System.out.println(new Main().coinChange(new int[]{2}, 4));
 //        System.out.println(new Main().coinChange(new int[]{1,2,5}, 11));
 //        System.out.println(new Main().coinChange(new int[]{2}, 1));
-        System.out.println(new Main().lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+//        System.out.println(new Main().lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+//        System.out.println(new Main().wordBreak("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat")));
+//        System.out.println(new Main().searchMatrix2(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 13));
+        System.out.println(new Main().searchInsert(new int[]{1, 3, 5, 6}, 2));
     }
 }
